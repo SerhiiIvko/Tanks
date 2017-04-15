5fa03791ced954830b0fa5308a37641f48502a42
Sergii <sergii.ivko@gmail.com>
2017-04-15 12:48:40 +0300

    MAIN-1

---
+++ src/com/thebyteguru/game/Entity.java
@@ -0,0 +1,23 @@
+package com.thebyteguru.game;
+
+import java.awt.Graphics2D;
+
+import com.thebyteguru.IO.Input;
+
+public abstract class Entity {
+
+	public final EntityType	type;
+
+	protected float			x;
+	protected float			y;
+
+	protected Entity(EntityType type, float x, float y) {
+		this.type = type;
+		this.x = x;
+		this.y = y;
+	}
+
+	protected abstract void update(Input input);
+
+	protected abstract void render(Graphics2D g);
+}
